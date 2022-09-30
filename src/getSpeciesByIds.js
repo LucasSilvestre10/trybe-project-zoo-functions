97fe6eca6c088e3c60de101a0cc9ebbf16c37045
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-09-30 14:58:28 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...params) {
+  let result = [];
+  if (params.length >= 1) {
+    result = data.species.filter(({ id }) => params.includes(id));
+  }
+  return result;
 }
+
+/* console.log(
+  getSpeciesByIds(
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+  )
+); */
 
 module.exports = getSpeciesByIds;
