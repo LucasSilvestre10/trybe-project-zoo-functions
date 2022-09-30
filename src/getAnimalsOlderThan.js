fd6c6cd0ef260257603bee8ec5d5ad3cbc004798
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-09-30 15:44:14 -0300

    requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const result = data.species
+    .filter((specie) => specie.name === animal)[0]
+    .residents.every((resident) => resident.age >= age);
+  return result;
 }
 
 module.exports = getAnimalsOlderThan;
