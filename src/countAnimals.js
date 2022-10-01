3e35eb21858643ad7dd3cac7b52e8bc87f893be4
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-01 19:00:27 -0300

    requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
+function countAll() {
+  const result = {};
+  data.species.forEach((element) => {
+    result[element.name] = element.residents.length;
+  });
+  return result;
+}
+
+function animalCountBySex(params) {
+  let result = 0;
+  const animals = data.species.find((element) => element.name === params.specie);
+  result = animals.residents.filter((sex) => sex.sex === params.sex).length;
+  return result;
+}
+
+function animalCountBySpecies(params) {
+  let result = 0;
+  result = data.species.find((element) => element.name === params.specie).residents.length;
+  return result;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return countAll();
+  } if (animal.sex) {
+    return animalCountBySex(animal);
+  }
+  return animalCountBySpecies(animal);
 }
 
 module.exports = countAnimals;
