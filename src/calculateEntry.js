a55015374210d76e3cbfd46e4d53b2e5d21caf61
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-03 19:24:32 -0300

    requisito 8 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      result.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      result.adult += 1;
+    } else {
+      result.senior += 1;
+    }
+  });
+
+  return result;
 }
 
 function calculateEntry(entrants) {
-// seu código aqui
+  let result = 0;
+
+  if (entrants && Object.keys(entrants).length !== 0) {
+    const count = countEntrants(entrants);
+    result = prices.child * count.child + prices.adult * count.adult + prices.senior * count.senior;
+  }
+
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
