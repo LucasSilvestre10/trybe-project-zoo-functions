a3cdf6b8ee1348ee21bebcf7ce964c6bd8348dc1
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-03 18:17:48 -0300

    requisito 7 e 15 feitos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,7 +16,22 @@ describe('Testes da função HandlerElephants', () => {
   it('testa se ao passar o parâmetro name retorna elephants', () => {
     expect(handlerElephants('name')).toBe('elephants');
   });
-  it('testa se ao passar o parâmetro names retorna um array com nomes', () => {
+  it('testa se ao passar o parâmetro names retorna um array com nomes dos elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
 });
