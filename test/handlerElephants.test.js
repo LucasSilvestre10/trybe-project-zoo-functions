e042bc28ccb1ec93ff9dc7e3fda0b2e69499c79a
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-03 18:12:02 -0300

    requisito 6 feito

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testa se ao não passar parâmetros retorna undefined', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+  it('teste de o tipo de parâmetro é uma string', () => {
+    expect(handlerElephants(0)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('testa se ao passar o parâmetro retorna um objeto', () => {
+    expect(typeof handlerElephants('elephants')).toBe('object');
+  });
+  it('testa se ao passar o parâmetro retorna uma string', () => {
+    expect(typeof handlerElephants('name')).toBe('string');
+  });
+  it('testa se ao passar o parâmetro name retorna elephants', () => {
+    expect(handlerElephants('name')).toBe('elephants');
+  });
+  it('testa se ao passar o parâmetro names retorna um array com nomes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+});
