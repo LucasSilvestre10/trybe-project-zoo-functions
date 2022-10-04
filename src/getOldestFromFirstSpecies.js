48350fbe634567ce13dc841377d0d9e5aa0ca308
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-10-04 01:04:21 -0300

    requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const fistSpecies = (id) => data.employees.find((emp) => emp.id === id).responsibleFor[0];
+
+const getOldest = (id) => {
+  const result = data.species
+    .filter((spec) => spec.id === id)[0]
+    .residents.reduce((a, b) => (b.age > a.age ? b : a));
+  return Object.values(result);
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  return getOldest(fistSpecies(id));
 }
 
 module.exports = getOldestFromFirstSpecies;
